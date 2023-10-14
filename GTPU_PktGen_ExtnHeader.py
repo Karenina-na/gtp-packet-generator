836a8b3019d8a28e8dcfe3b06a6df4607e66bb12
Karenina-na <weizixiang0@outlook.com>
2023-10-14 22:49:13 +0800

    format code

--- GTPU_PktGen_ExtnHeader.py
+++ GTPU_PktGen_ExtnHeader.py
@@ -1,7 +1,15 @@
 from scapy.contrib.gtp import (
-        GTP_U_Header,
-        GTPPDUSessionContainer)
+    GTP_U_Header,
+    GTPPDUSessionContainer)
 from scapy.all import *
 
-sendp(Ether()/IP(src="192.168.50.100", dst="192.168.50.200")/UDP(sport=2152,dport=2152)/GTP_U_Header(teid=100,next_ex=133)/GTPPDUSessionContainer(type=1, QFI=6)/IP(src="192.168.128.79", dst="8.8.8.8")/TCP(sport=5001,dport=53),iface="veth0", count=5)
+sendp(
+    Ether() /
+    IP(src="192.168.85.1", dst="192.168.85.100") /
+    UDP(sport=2152, dport=2152)/GTP_U_Header(teid=100, next_ex=133) /
+    GTPPDUSessionContainer(type=1, QFI=6) /
+    IP(src="192.168.10.10", dst="8.8.8.8") /
+    TCP(sport=5001, dport=53),
 
+    iface="VMware Network Adapter VMnet1", count=5
+)
