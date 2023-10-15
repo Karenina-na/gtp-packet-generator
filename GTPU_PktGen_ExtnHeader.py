5187c70bd4f04c7f67630258d95ab37ea9b0437f
Karenina-na <weizixiang0@outlook.com>
2023-10-15 17:30:26 +0800

    Add DNS query sample

--- GTPU_PktGen_ExtnHeader.py
+++ GTPU_PktGen_ExtnHeader.py
@@ -1,15 +1,18 @@
 from scapy.contrib.gtp import (
     GTP_U_Header,
     GTPPDUSessionContainer)
+from scapy.layers.inet import IP, UDP, Ether
+from scapy.layers.dns import DNS, DNSQR
 from scapy.all import *
 
+# DNS Query Packet
 sendp(
     Ether() /
-    IP(src="192.168.85.1", dst="192.168.85.100") /
-    UDP(sport=2152, dport=2152)/GTP_U_Header(teid=100, next_ex=133) /
-    GTPPDUSessionContainer(type=1, QFI=6) /
-    IP(src="192.168.10.10", dst="8.8.8.8") /
-    TCP(sport=5001, dport=53),
-
-    iface="VMware Network Adapter VMnet1", count=5
+    IP(src="127.0.10.100", dst="127.0.1.100") /
+    UDP(sport=2152, dport=2152) /
+    GTP_U_Header(teid=0x00000001) /
+    IP(src="172.16.0.2", dst="8.8.8.8") /
+    UDP(sport=53, dport=53) /
+    DNS(rd=1, qd=DNSQR(qname="connectivitycheck.gstatic.com", qtype="A")),
+    iface="VMware Network Adapter VMnet1", count=1
 )
