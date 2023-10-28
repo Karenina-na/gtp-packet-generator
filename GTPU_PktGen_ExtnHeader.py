50522f054999cccd20824ad3a973c15b1556a4f4
Karenina-na <weizixiang0@outlook.com>
2023-10-28 11:43:13 +0800

    add gtp-dns for example

--- GTPU_PktGen_ExtnHeader.py
+++ GTPU_PktGen_ExtnHeader.py
@@ -7,12 +7,12 @@ from scapy.all import *
 
 # DNS Query Packet
 sendp(
-    Ether() /
-    IP(src="127.0.10.100", dst="127.0.1.100") /
+    Ether(src='00:00:00:00:00:00', dst='00:00:00:00:00:00') /
+    IP(src="127.0.1.1", dst="127.0.1.100", flags="DF", id=0x005678) /
     UDP(sport=2152, dport=2152) /
-    GTP_U_Header(teid=0x00000001) /
-    IP(src="172.16.0.2", dst="8.8.8.8") /
-    UDP(sport=53, dport=53) /
-    DNS(rd=1, qd=DNSQR(qname="connectivitycheck.gstatic.com", qtype="A")),
-    iface="VMware Network Adapter VMnet1", count=1
+    GTP_U_Header(teid=0x00000005) /
+    IP(src="172.16.0.5", dst="8.8.8.8", flags="DF") /
+    UDP(sport=5678, dport=53) /
+    DNS(id=0x1234, rd=1, qd=DNSQR(qname="www.baidu.com", qtype="A")),
+    iface="wlp9s0", count=1
 )
